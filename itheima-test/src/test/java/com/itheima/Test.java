f9419b6692851c86b6c98f489101fd15d45d8dc5
ky <ky@ky.com>
2019-04-29 11:53:07 +0800

    idea中第二次提交

--- itheima-test/src/test/java/com/itheima/Test.java
+++ itheima-test/src/test/java/com/itheima/Test.java
@@ -1,7 +1,7 @@
 package com.itheima;
 
-public class Git {
+public class Test {
     public static void main(String[] args) {
-        System.out.println("你好 git2");
+        System.out.println("测试test");
     }
 }
